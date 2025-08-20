12877cb4c53e8a9d5d43569ec65fc0c22e1e9a2e
alexsander ribeiro <102040731+Sander0026@users.noreply.github.com>
2025-08-20 09:14:45 -0300

    Criando estrutura do projeto

---
+++ Simulado_Gravitacional_Progaramacao_Avancada/Models/Corpo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulado_Gravitacional_Progaramacao_Avancada.Models
+{
+    class Corpo
+    {
+        public string Nome { get; set; }
+        public double Massa { get; set; }
+        public double Raio { get; set; }
+        public double Densidade { get; set; }
+        public double PosX { get; set; }
+        public double PosY { get; set; }
+        public double VelX { get; set; }
+        public double VelY { get; set; }
+
+        public static Corpo operator +(Corpo c1, Corpo c2)
+        {
+            Corpo cp = new Corpo();
+            cp.Massa = c1.Massa + c2.Massa;
+
+            return cp;
+        }
+
+        
+
+    }
+}
